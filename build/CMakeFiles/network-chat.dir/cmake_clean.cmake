bc2d98b1ac49c02416bfab4e44888e0f1056ea41
DimitriiTrater <dima1603040@gmail.com>
2023-01-19 19:36:31 +0200

    Init commit

---
+++ build/CMakeFiles/network-chat.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/network-chat.dir/main.cpp.obj"
+  "CMakeFiles/network-chat.dir/main.cpp.obj.d"
+  "libnetwork-chat.dll.a"
+  "network-chat.exe"
+  "network-chat.exe.manifest"
+  "network-chat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/network-chat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <SFML\Network.hpp>
+#include <SFML\Window.hpp>
+
+int main(int, char**) 
+{   
+    return 0;
+}
