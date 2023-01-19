17fefa5acf7c84dc8ac2104665b8f39b2d98d2b1
DimitriiTrater <dima1603040@gmail.com>
2023-01-19 20:10:20 +0200

    Delete build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/1/CLionProjects/network-chat
-# Build directory: C:/Users/1/CLionProjects/network-chat/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- main.cpp
+++ main.cpp
@@ -4,5 +4,64 @@
 
 int main(int, char**) 
 {   
+    char type;
+
+    std::string name;
+    std::string message = "";
+
+
+    sf::TcpSocket tcpSocket;
+    sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+    
+
+    std::cout << "Enter type of connection: [c] - client, [s] - server" << std::endl;
+    std::cin  >> type;
+
+    if (type == 's')
+    {
+        sf::TcpListener tcpListener;
+        tcpListener.listen(2000);
+
+        if (tcpListener.accept(tcpSocket) != sf::Socket::Done)
+            std::cerr << "Error!" << std::endl;
+    }
+    else if (type == 'c')
+    {
+        if (tcpSocket.connect(ip, 2000) != sf::Socket::Done)
+            std::cerr << "Error!" << std::endl;
+    }
+
+
+    std::cout << "Enter your name" << std::endl;
+    std::cin  >> name;
+
+    tcpSocket.setBlocking(false);
+
+    sf::Packet tcpPacket;
+
+    while (true)
+    {
+        std::cin >> message;
+
+        if (message != "")
+        {
+            tcpPacket.clear();
+            tcpPacket << name << message;
+
+            tcpSocket.send(tcpPacket);
+
+            message = "";
+        }
+
+        if (tcpSocket.receive(tcpPacket) == sf::Socket::Done)
+        {
+            std::string nameRec;
+            std::string messageRec;
+
+            tcpPacket    >> nameRec >> messageRec;
+            std::cout << nameRec << ": " << messageRec << std::endl;
+        }
+    }
+
     return 0;
 }
